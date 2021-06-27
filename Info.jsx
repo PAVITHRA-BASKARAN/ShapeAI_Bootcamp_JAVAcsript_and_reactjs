bd1ce2aa6caff1093f75b9af8090b58175fad434
PAVITHRA-BASKARAN <71811420+PAVITHRA-BASKARAN@users.noreply.github.com>
2021-06-27 19:13:44 +0530

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Info() {
+  return (
+    <div className= "note">
+    <h1> JAVA Programming </h1>
+    <p> Introduction to JAVA Programming </p>
+    </div>
+    );
+}
+
+export default Info;
